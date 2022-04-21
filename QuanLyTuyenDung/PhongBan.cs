44b6d442e5fb94202d93ece195fe6b31cefbe7d6
vantung <vantung2102@gmail.com>
2022-04-21 21:07:21 +0700

    add class

---
+++ QuanLyTuyenDung/PhongBan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLyTuyenDung
+{
+    class PhongBan
+    {
+        private int IDPhongBan;
+        private string Ten;
+        public PhongBan(int IDPhongBan, string Ten)
+        {
+        }
+        public int IDPhongBan1 { get => IDPhongBan; set => IDPhongBan = value; }
+        public string Ten1 { get => Ten; set => Ten = value; }
+    }
+}
